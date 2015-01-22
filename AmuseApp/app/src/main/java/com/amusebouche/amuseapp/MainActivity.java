04e97c8d9ec2dad66f879dfb3fa2042836c3f4f4
Noelia Sales <noelia.salesmontes@gmail.com>
2015-01-22 16:41:43 +0100

    Adding search button to title bar. I18N.

--- AmuseApp/app/src/main/java/com/amusebouche/amuseapp/MainActivity.java
+++ AmuseApp/app/src/main/java/com/amusebouche/amuseapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.amusebouche.amuseapp;
 import android.app.Activity;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -44,17 +45,17 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                search();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void search() {
+        Log.v("INFO", "Search clicked");
     }
 }
 
