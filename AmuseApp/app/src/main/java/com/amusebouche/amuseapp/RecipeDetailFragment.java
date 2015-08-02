839d1419e6b220891d87783474b108dd3ce9a227
Noelia Sales Montes <noelia.salesmontes@gmail.com>
2015-08-02 12:48:58 +0200

    Fix splash loading indicator.

--- AmuseApp/app/src/main/java/com/amusebouche/amuseapp/RecipeDetailFragment.java
+++ AmuseApp/app/src/main/java/com/amusebouche/amuseapp/RecipeDetailFragment.java
@@ -11,7 +11,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.ActivityOptionsCompat;
 import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
