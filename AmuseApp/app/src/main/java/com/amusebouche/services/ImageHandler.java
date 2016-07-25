f75d0dd1bff61ec1931d13fe2ef36c7d99f9d20b
Noelia Sales <noelia.salesmontes@gmail.com>
2016-07-25 20:22:58 +0200

    Update graphics.

--- AmuseApp/app/src/main/java/com/amusebouche/services/ImageHandler.java
+++ AmuseApp/app/src/main/java/com/amusebouche/services/ImageHandler.java
@@ -45,36 +45,20 @@ public class ImageHandler {
     public static void setCellImage(Context context, String imageName, ImageView imageView,
                                     final ProgressBar progressBar) {
         // Get a random default image
-        // TODO: Update these images with new ones
         Random r = new Random();
-        int randomNumber = (r.nextInt(8));
+        int randomNumber = (r.nextInt(3));
         int resource;
 
         switch (randomNumber) {
             default:
             case 0:
-                resource = R.drawable.sample_0;
+                resource = R.drawable.fake_food_1;
                 break;
             case 1:
-                resource = R.drawable.sample_1;
+                resource = R.drawable.fake_food_2;
                 break;
             case 2:
-                resource = R.drawable.sample_2;
-                break;
-            case 3:
-                resource = R.drawable.sample_3;
-                break;
-            case 4:
-                resource = R.drawable.sample_4;
-                break;
-            case 5:
-                resource = R.drawable.sample_5;
-                break;
-            case 6:
-                resource = R.drawable.sample_6;
-                break;
-            case 7:
-                resource = R.drawable.sample_7;
+                resource = R.drawable.fake_food_3;
                 break;
         }
 
